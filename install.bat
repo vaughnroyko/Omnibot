9ac3bf4f73c15b03f564da438d43c7a0eb6fc9f3
Mackenzie McClane <aaritak@gmail.com>
2015-06-23 14:07:48 -0500

    Better installing

--- install.bat
+++ install.bat
@@ -1,8 +1,16 @@
 call npm install
 @echo off
-@echo cd .. > run.bat
+@echo cd database > run.bat
+@echo call stop.bat >> run.bat
+@echo call start.bat >> run.bat
+@echo cd ../.. >> run.bat
 @echo node DrathybotAlpha >> run.bat
 @echo cd DrathybotAlpha >> run.bat
 @echo pause >> run.bat
+mkdir database
+cd database
+@echo del db.log > start.bat
+@echo start "MongoDB" mongod --dbpath %%~dp0 --logpath db.log >> start.bat
+@echo mongo localhost:27017 --eval "db.adminCommand({shutdown : 1})" > stop.bat
+cd ..
 pause
-run.bat
