fb22415dbb2cb7cd81a80667c181d1850864e644
Mackenzie McClane <aaritak@gmail.com>
2015-06-23 22:20:20 -0500

    Now with 100% clean startup!

--- install.bat
+++ install.bat
@@ -10,23 +10,23 @@ call npm install
 @echo ) >> run.bat
 @echo cd database >> run.bat
 @echo start /WAIT restart.bat >> run.bat
-@echo cd ../.. >> run.bat
-@echo node DrathybotAlpha >> run.bat
-@echo cd DrathybotAlpha >> run.bat
-@echo pause >> run.bat
+@echo cd .. >> run.bat
+@echo start "Drathybot" node ../DrathybotAlpha >> run.bat
 
 mkdir database
 cd database
 
+@echo CreateObject("Wscript.Shell").Run "" ^& WScript.Arguments(0) ^& "", 0, False > i.vbs
+
 @echo del db.log > start.bat
-@echo start "MongoDB" mongod --dbpath %%~dp0 --logpath db.log >> start.bat
+@echo mongod --dbpath %%~dp0 --logpath db.log >> start.bat
 
 @echo mongo localhost:27017 --eval "db.adminCommand({shutdown : 1})" > stop.bat
 
 @echo call stop.bat > restart.bat
-@echo pause >> restart.bat
-@echo call start.bat >> restart.bat
+@echo timeout 1 >> restart.bat
+@echo wscript.exe i.vbs "start.bat" >> restart.bat
 @echo exit >> restart.bat
 
 cd ..
-pause
+timeout 5
