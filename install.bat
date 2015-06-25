b853efb52786e5e8f859630bd51d13d04a8a172d
Mackenzie McClane <aaritak@gmail.com>
2015-06-24 23:17:33 -0500

    How's dis

--- install.bat
+++ install.bat
@@ -1,34 +1,50 @@
-call npm install
-
 @echo off
 
-@echo if "%%~1"=="-FIXED_CTRL_C" ( > s.bat
-@echo    shift >> s.bat
-@echo ) else ( >> s.bat
-@echo    call ^<NUL %%0 -FIXED_CTRL_C %%* >> s.bat
-@echo    goto :EOF >> s.bat
-@echo ) >> s.bat
-@echo cd database >> s.bat
-@echo i.vbs "start /WAIT restart.bat" >> s.bat
-@echo cd .. >> s.bat
-@echo start "Drathybot" node %~dp0 >> s.bat
+setlocal
+:askMongoFolder
+set /p loc="Please enter the path of MongoDB's executable containing folder: "
+if "%loc%"=="" set loc=C:/Program Files/MongoDB/Server/3.0/bin
+if NOT "%loc:~-1%"=="/" set loc=%loc%/
+if NOT exist "%loc%mongo.exe" goto askMongoFolder
+if NOT exist "%loc%mongod.exe" goto askMongoFolder
+echo %loc% > "database/mongo.loc"
+endlocal
 
-@echo CreateObject("Wscript.Shell").Run "s.bat", 0, True > Drathybot.vbs
+call npm install
+
+echo if "%%~1"=="-FIXED_CTRL_C" ( > s.bat
+echo   shift >> s.bat
+echo ) else ( >> s.bat
+echo   call ^<NUL %%0 -FIXED_CTRL_C %%* >> s.bat
+echo   goto :EOF >> s.bat
+echo ) >> s.bat
+echo cd database >> s.bat
+echo i.vbs "start /WAIT restart.bat" >> s.bat
+echo cd .. >> s.bat
+echo start "Drathybot" node %~dp0 >> s.bat
+
+echo CreateObject("Wscript.Shell").Run "s.bat", 0, True > Drathybot.vbs
 
 mkdir database
 cd database
 
-@echo CreateObject("Wscript.Shell").Run "" ^& WScript.Arguments(0) ^& "", 0, False > i.vbs
+echo CreateObject("Wscript.Shell").Run "" ^& WScript.Arguments(0) ^& "", 0, False > i.vbs
 
-@echo del db.log > start.bat
-@echo "C:/Program Files/MongoDB/Server/3.0/bin/mongod.exe" --dbpath %%~dp0 --logpath db.log >> start.bat
+echo del db.log > start.bat
+echo setlocal >> start.bat
+echo (set /p loc=) ^< mongo.loc >> start.bat
+echo "%%loc%%/mongod.exe" --dbpath %%~dp0 --logpath db.log >> start.bat
+echo endlocal >> start.bat
 
-@echo "C:/Program Files/MongoDB/Server/3.0/bin/mongo.exe" localhost:27017 --eval "db.adminCommand({shutdown : 1})" > stop.bat
+echo setlocal > stop.bat
+echo (set /p loc=) ^< mongo.loc >> stop.bat
+echo "%%loc%%/mongo.exe" localhost:27017 --eval "db.adminCommand({shutdown : 1})" >> stop.bat
+echo endlocal >> stop.bat
 
-@echo call stop.bat > restart.bat
-@echo timeout 1 >> restart.bat
-@echo wscript.exe i.vbs "start.bat" >> restart.bat
-@echo exit >> restart.bat
+echo call stop.bat > restart.bat
+echo timeout 1 >> restart.bat
+echo wscript.exe i.vbs "start.bat" >> restart.bat
+echo exit >> restart.bat
 
 cd ..
 timeout 5
