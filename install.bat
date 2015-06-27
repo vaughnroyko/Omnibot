768eeda4efecafaa95d4c0a8307be45d8a9f7eff
Mackenzie McClane <aaritak@gmail.com>
2015-06-27 02:49:45 -0500

    Tell what's going on during installation

--- install.bat
+++ install.bat
@@ -1,16 +1,29 @@
 @echo off
 
+echo Welcome to the installer for Drathybot!
+timeout 3 > NUL
+echo Before we begin, we need the path of MongoDB's executables.
+timeout 2 > NUL
+echo For example, the default install is C:/Program Files/MongoDB/Server/3.0/bin
+timeout 2 > NUL
+echo You can press [Enter] if that is your path.
+timeout 2 > NUL
+
 setlocal
 :askMongoFolder
 set /p loc="Please enter the path of MongoDB's executable containing folder: "
 if "%loc%"=="" set loc=C:/Program Files/MongoDB/Server/3.0/bin
-if NOT "%loc:~-1%"=="/" set loc=%loc%/
+if NOT "%loc:~-1%" == "/" set loc=%loc%/
 if NOT exist "%loc%mongo.exe" goto askMongoFolder
 if NOT exist "%loc%mongod.exe" goto askMongoFolder
 echo %loc% > "database/mongo.loc"
 endlocal
 
+echo Installing modules...
 call npm install
+
+echo Finished installing modules.
+echo Creating more batch files...
 
 echo if "%%~1"=="-FIXED_CTRL_C" ( > s.bat
 echo   shift >> s.bat
@@ -45,6 +58,8 @@ echo call stop.bat > restart.bat
 echo timeout 1 >> restart.bat
 echo wscript.exe i.vbs "start.bat" >> restart.bat
 echo exit >> restart.bat
+
+echo Drathybot is finished installing!
 
 cd ..
 timeout 5
